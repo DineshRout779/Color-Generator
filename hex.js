e4b290549a2445e8036b8af45428b386719c7e65
DineshRout779 <rddinesh779@gmail.com>
2020-10-11 18:16:36 +0530

    hex updated

---
+++ hex.js
@@ -0,0 +1,22 @@
+const arr = [
+    {
+      bgcolor: "#264653",
+      body_bgcolor: "#264653"
+    },
+    {
+      bgcolor: "#2A9D8F",
+      body_bgcolor: "#2A9D8F"
+    },
+    {
+      bgcolor: "",
+      body_bgcolor: ""
+    },
+    {
+      bgcolor: "",
+      body_bgcolor: ""
+    },
+    {
+      bgcolor: "",
+      body_bgcolor: ""
+    }
+  ];
