39803dc2cbb1435d02d2ee641e77b09598d73d9a
DineshRout779 <rddinesh779@gmail.com>
2020-10-11 21:28:33 +0530

    app working

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let bgc = document.querySelector(".color");
 let color_name = document.querySelector(".hex");
 let next = document.querySelector(".next");
+let copy = document.querySelector(".copy");
 
 window.onload = function(){
     let random = Math.floor(Math.random() * arr.length);
-    bgc.style.backgroundColor = arr[random].bgcolor;
-    color_name.innerText = arr[random].bgcolor;
+    document.body.style.backgroundColor = arr[random].bgcolor;
+    color_name.innerText =arr[random].bgcolor;
 }
 
 next.addEventListener("click", function (e) {
     e.preventDefault();
     let random = Math.floor(Math.random() * arr.length);
-    bgc.style.backgroundColor = arr[random].bgcolor;
-    color_name.innerText = arr[random].bgcolor;
+    document.body.style.backgroundColor = arr[random].bgcolor;
+    color_name.innerText =arr[random].bgcolor;
 })
+
+copy.addEventListener("click", copyText)
+
+function copyText(){
+    let text = color_name.innerText;
+    alert("copied!")
+}
+
